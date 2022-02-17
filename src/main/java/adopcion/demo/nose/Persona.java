03fa14920adbd1c355c65b13bfb388931cd3acf6
luisn <luisn@DESKTOP-9E80TUC>
2022-02-17 11:13:48 -0600

    HolaMundo

---
+++ src/main/java/adopcion/demo/nose/Persona.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adopcion.demo.nose;
+
+import com.sun.tools.javac.Main;
+
+/**
+ *
+ * @author luisn
+ */
+public class Persona {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
